99d817d52d60119a28eaccc6964e60d2f6574970
Fei Wang <silviafey.wang@utoronto.ca>
2021-06-23 16:30:59 +0800

    Fixed bugs in the FedAtt and added noise for evaluating privacy preservation.

--- examples/fedatt/fedatt_server.py
+++ examples/fedatt/fedatt_server.py
@@ -19,34 +19,47 @@ import torch.nn.functional as F
 class Server(fedavg.Server):
     """A federated learning server using the FedAtt algorithm."""
     async def federated_averaging(self, updates):
+        epsilon = 1.0
+        noise = 0.001
         """Aggregate weight updates from the clients using FedAtt."""
         # Extract weights from the updates
         weights_received = self.extract_client_updates(updates)
 
-        # Performing attentive aggregating
+        # Extract baseline model weights
+        baseline_weights = self.algorithm.extract_weights()
+        
+        # Update server weights
+        update = self.avg_att(baseline_weights, weights_received)
+
+        return update
+    
+    
+    def avg_att(self, baseline_weights, weights_received, epsilon=1.0, dp=0.001):
+        """Perform attentive aggregation with the attention mechanism.
+            epsilon: step size for aggregation
+            dp: magnitude of normal noise in the randomization mechanism
+        """
         att_update = {
             name: self.trainer.zeros(weights.shape)
             for name, weights in weights_received[0].items()
         }
 
-        # Extract baseline model weights
-        baseline_weights = self.algorithm.extract_weights()
-
-        # Calculate attention
         atts = OrderedDict()
         for name, weight in baseline_weights.items():
             atts[name] = self.trainer.zeros(len(weights_received))
             for i, update in enumerate(weights_received):
                 delta = update[name]
                 atts[name][i] = torch.linalg.norm(weight - delta)
+        
+        for name, weight in baseline_weights.items():
             atts[name] = F.softmax(atts[name], dim=0)
 
         for name, weight in baseline_weights.items():
             att_weight = self.trainer.zeros(weight.shape)
             for i, update in enumerate(weights_received):
                 delta = update[name]
-                att_weight += (weight - delta).mul(atts[name][i])
+                att_weight += torch.mul(weight - delta, atts[name][i])
 
-            att_update[name] = -att_weight.mul(1.2)
+            att_update[name] = weight - torch.mul(att_weight, epsilon) + torch.mul(torch.randn(weight.shape), dp)
 
         return att_update
